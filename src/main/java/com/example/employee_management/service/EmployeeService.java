57c461b7d6b2c3efa6c9ecc1bf01cc3179037669
pnt2005 <phannthanh2005@gmail.com>
2026-08-05 08:17:39 +0700

    add employee service

---
+++ src/main/java/com/example/employee_management/service/EmployeeService.java
@@ -0,0 +1,57 @@
+package com.example.employee_management.service;
+
+import com.example.employee_management.model.EmployeeEntity;
+import com.example.employee_management.repository.DepartmentRepository;
+import com.example.employee_management.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
+    public EmployeeService(EmployeeRepository employeeRepository,
+                           DepartmentRepository departmentRepository) {
+        this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<EmployeeEntity> getEmployee(Map<String, String> params) {
+        List<EmployeeEntity> employeeEntityList = new ArrayList<>();
+        String name = params.getOrDefault("name", null);
+        String department = params.getOrDefault("department", null);
+        if (!name.isEmpty()) {
+            employeeEntityList = employeeRepository.findAllByName(name);
+        }
+        else if (!department.isEmpty()) {
+            employeeEntityList = employeeRepository.findAllByDepartment(department);
+        }
+        else {
+            employeeEntityList = employeeRepository.findAll();
+        }
+        return employeeEntityList;
+    }
+
+    public EmployeeEntity createEmployee(Map<String, String> request) {
+        EmployeeEntity employeeEntity = new EmployeeEntity();
+        employeeEntity.setEmail(request.get("email"));
+        employeeEntity.setName(request.get("name"));
+        employeeEntity.setDepartmentEntity(departmentRepository.getByName(request.get("department")));
+        return employeeRepository.save(employeeEntity);
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepository.deleteById(id);
+    }
+
+    public EmployeeEntity updateEmployee(Long id, Map<String, String> request) {
+        EmployeeEntity employeeEntity = employeeRepository.findById(id).get();
+        employeeEntity.setName(request.get("name"));
+        employeeEntity.setEmail(request.get("email"));
+        employeeEntity.setDepartmentEntity(departmentRepository.getByName(request.get("department")));
+        return employeeEntity;
+    }
+}
