de28a84cb2b17f184d263306db49fdbede85cbe0
pnt2005 <phannthanh2005@gmail.com>
2026-08-07 11:01:51 +0700

    add schedule logging

--- src/main/java/com/example/employee_management/EmployeeManagementApplication.java
+++ src/main/java/com/example/employee_management/EmployeeManagementApplication.java
@@ -7,11 +7,14 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
@@ -23,5 +26,10 @@ public class EmployeeManagementApplication {
 		CaffeineCacheManager cacheManager = new CaffeineCacheManager("employee");
 		cacheManager.setCaffeine(Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.MINUTES));
 		return cacheManager;
+	}
+
+	@Scheduled(fixedRate = 30000)
+	public void systemRunning() {
+		System.out.println("system running");
 	}
 }
